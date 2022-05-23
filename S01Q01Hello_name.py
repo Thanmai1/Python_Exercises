475e0cc0aa57b1036e6eb1e492d391888099c394
Thanmai1 <Thanmai1@users.noreply.github.com>
2022-05-23 13:22:16 -0400

    Add files via upload

---
+++ S01Q01Hello_name.py
@@ -0,0 +1,11 @@
+'''Re-write the earlier question of S01Q01 using functions as mentioned below :
+
+  - Modify the first “Hello World” program to prompt for user’s name 
+           and then wish the user by saying Hello followed by his name
+
+           Example :
+           If user's name is Sam, then expected output is :
+           Hello Sam !!! '''
+
+name = input("Enter your Name here:")
+print("Hello",name)
