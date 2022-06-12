88a18a83977a28134488a35f940326c68b0f45b4
Thanmai1 <Thanmai1@users.noreply.github.com>
2022-06-12 06:59:28 -0400

    Add files via upload
    Using functions

---
+++ S01AQ02multiplication_table17.py
@@ -0,0 +1,14 @@
+'''- Print the multiplication table of 17'''
+
+def table17():
+    
+    i = 1
+
+    while i <= 10:
+        res = 17 * i
+        print( "17 x", i,"=", res)
+        i = i + 1
+
+#Main starts here
+
+table17()
